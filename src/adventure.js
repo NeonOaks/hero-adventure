63909155c0fd6dba54aadf6957ab083bd3840240
NeonOaks <davi.oaks@gmail.com>
2025-04-14 21:58:46 -0300

    Fix: heroCreate

--- src/adventure.js
+++ src/adventure.js
@@ -3,36 +3,49 @@ import { stdin as input, stdout as output } from 'process';
 
 class adventure {
     constructor(actionUser, heroCreate, chooseFaction,  executionAttack) {
-                this.actionUser = actionUser;
-                this.heroCreate = heroCreate;
-                this.chooseFaction = chooseFaction;
-                this.executionAttack = executionAttack;
+                r1 = readline.createInterface({ input, output});
+                this.startGame;
     }
 
-    r1 = readline.createInterface({ input, output});
+   startGame() {
+        this.actionUser();
+   }
 
     actionUser(){
-        this.r1.question('Qual o nome do Heroi? ' ,(this.nameHero) => {
-            console.log(`Seu nome é ${nameHero}`);
-        })
-
-        
+        this.r1.question('Qual o nome do Heroi? ' ,(name) => {
+            this.r1.question('Qual sua idade: ', (age) => {
+                this.heroCreate(name,age);
+                this.askFaction();
+            })
+        });    
     }
 
-    heroCreate(){
-        nameHero = nameHero;
-        age = age;
+    heroCreate(name,age){
+        this.nameHero = nameHero;
+        this.age = age;
+        console.log(`Nome do Héroi: ${this.nameHero}`);
+        console.log(`Idade: ${this.age}`);
     }
 
     chooseFaction() {
         switch (this.chooseFaction.faction()) {
             case '1':
-                faction = ''
+                faction = 'Mago';
+                break;
+            case '2':
+                faction = 'Guerreiro';
+                break;
+            case '3':
+                faction = 'Monge';
+                break;
+            case '4':
+                faction = 'Ninja';
+                break;
+            default:
+                console.log('Opção Inválida.');
+            this.r1.close;
+            return;
         }
-         wizard = wizard;
-        warrior = warrior;
-        monk = monk;
-        ninja = ninja;
     }
 
     executionAttack() {
